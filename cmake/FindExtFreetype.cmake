9500b7a8aed553e2bf182198e5ecef9d998409a5
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-02-16 12:59:46 +0300

    Update to borsch 2

--- cmake/FindExtFreetype.cmake
+++ cmake/FindExtFreetype.cmake
@@ -2,9 +2,11 @@
 # Project:  external projects
 # Purpose:  CMake build scripts
 # Author:   Alexander Lisovenko, alexander.lisovenko@gmail.com
+# Author:   Dmitry Baryshnikov, dmitry.baryshnikov@nextgis.com
 ################################################################################
-# Copyright (C) 2016, NextGIS <info@nextgis.com>
+# Copyright (C) 2016-2018, NextGIS <info@nextgis.com>
 # Copyright (C) 2016 Alexander Lisovenko
+# Copyright (C) 2018 Dmitry Baryshnikov
 #
 # This script is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,6 +22,6 @@
 # along with this script.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-set(repo_name lib_freetype)
-set(repo_project freetype)
-set(repo_include )
+set(repo nextgis-borsch/lib_freetype)
+set(repo_type github)
+# set(repo_branch master) # No need to set as master is default value
